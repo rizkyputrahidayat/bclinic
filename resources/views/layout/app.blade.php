30ba92c9402c13d042c09f5a2191cc26018ba114
User <macos@macOSs-MacBook-2.local>
2023-06-23 15:07:09 +0700

    update dikit

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -125,7 +125,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                 <li class="nav-item">
                                     <a href="/face" class="nav-link">
                                         <i class="far fa-circle nac-icon"></i>
-                                        <p>Face</p>
+                                        <p>Face Body</p>
                                     </a>
                                 </li>
                             </ul>
@@ -134,14 +134,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                     <a href="/facial" class="nav-link">
                                         <i class="far fa-circle nac-icon"></i>
                                         <p>Facial</p>
-                                    </a>
-                                </li>
-                            </ul>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="/stories" class="nav-link">
-                                        <i class="far fa-circle nac-icon"></i>
-                                        <p>Story</p>
                                     </a>
                                 </li>
                             </ul>
